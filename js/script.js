fc3596caece8740e7064d16608fda0d43ac2057e
Nikita <zelenkov4462@gmail.com>
2022-04-07 13:53:47 +0800

    60 axios

--- js/script.js
+++ js/script.js
@@ -201,8 +201,21 @@ document.addEventListener("DOMContentLoaded", () => {
     return await res.json();
   };
 
-  getResourse("http://localhost:3000/menu").then((data) => {
-    data.forEach(({ img, altimg, title, descr, price }) => {
+  // getResourse("http://localhost:3000/menu").then((data) => {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     new MenuCard(
+  //       img,
+  //       altimg,
+  //       title,
+  //       descr,
+  //       price,
+  //       ".menu .container"
+  //     ).render();
+  //   });
+  // });
+
+  axios.get("http://localhost:3000/menu").then((data) => {
+    data.data.forEach(({ img, altimg, title, descr, price }) => {
       new MenuCard(
         img,
         altimg,
